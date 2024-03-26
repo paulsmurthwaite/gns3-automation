db7b135edc0556072694827c031cd0703e23c598
paul-smurthwaite <psmurthw@outlook.com>
2024-03-26 17:27:28 +0000

    updating section 5

--- python3-networking/section_5-napalm/napalm1a.py
+++ python3-networking/section_5-napalm/napalm1a.py
@@ -1,6 +1,6 @@
 from napalm import get_network_driver
 driver = get_network_driver('ios')
-iosvl2 = driver('192.168.122.72', 'psmurthw', 'cisco')
+iosvl2 = driver('192.168.122.72', 'lab', 'cisco')
 iosvl2.open()
 
 ios_output = iosvl2.get_facts()
--- python3-networking/section_5-napalm/napalm2a.py
+++ python3-networking/section_5-napalm/napalm2a.py
@@ -1,7 +1,7 @@
 import json
 from napalm import get_network_driver
 driver = get_network_driver('ios')
-iosvl2 = driver('192.168.122.72', 'psmurthw', 'cisco')
+iosvl2 = driver('192.168.122.72', 'lab', 'cisco')
 iosvl2.open()
 
 ios_output = iosvl2.get_mac_address_table()
