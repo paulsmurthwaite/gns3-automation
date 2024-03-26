ed547fd91c3282f05ff317e7de4542ce4d0bf848
paul-smurthwaite <psmurthw@outlook.com>
2024-03-26 10:36:21 +0000

    adding project artifacts

---
+++ python3-networking/section_3-quick-start/README.md
@@ -0,0 +1,6 @@
+# Project Artifacts
+Udemy: Python Network Programming for Network Engineers (Python 3)
+https://www.udemy.com/course/python-network-programming-for-network-engineers-python-3
+
+## Section 3: Quick Start Guide to Network Automation
+- Initial GNS3 Topology
---
+++ python3-networking/section_3-quick-start/python31.py
@@ -0,0 +1,30 @@
+import getpass
+import telnetlib
+
+"""
+Author: Paul Smurthwaite
+Purpose: GNS3 Automation
+"""
+
+HOST = "192.168.122.71"
+user = input("Enter your telnet username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"enable\n")
+tn.write(b"cisco\n")
+tn.write(b"configure terminal\n")
+tn.write(b"interface Loopback0\n")
+tn.write(b"ip address 1.1.1.1 255.255.255.255\n")
+tn.write(b"end\n")
+tn.write(b"write\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
